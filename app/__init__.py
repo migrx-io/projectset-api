157bfa0684552bbe58aab4b0534ec49cb46e834b
anatolii2makaroff <anatolii.makaroff@gmail.com>
2024-03-18 23:47:06 -0400

    add chat page

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,7 @@ from app.webapp.login_page import login_page
 from app.webapp.repo_page import repo_page
 from app.webapp.projectset_page import projectset_page
 from app.webapp.projectset_template_page import projectset_template_page
+from app.webapp.chat_page import chat_page
 
 from app.util.pool import Pool
 from app.util.push_worker import push, loop_unfinished_tasks
@@ -134,6 +135,8 @@ app.register_blueprint(repo_page, url_prefix='/repo')
 app.register_blueprint(projectset_page, url_prefix='/projectset')
 app.register_blueprint(projectset_template_page,
                        url_prefix='/projectsettemplate')
+
+app.register_blueprint(chat_page, url_prefix='/chat')
 
 # Common errors
 app.register_error_handler(500, handle_internal_error)
---
+++ app/webapp/chat_page.py
@@ -0,0 +1,46 @@
+import logging as log
+from flask import Blueprint, render_template, request, jsonify
+from app.util.auth import jwt_required
+import json
+
+
+chat_page = Blueprint('chat_page', __name__)
+
+
+@chat_page.route('/', methods=['GET'])
+@jwt_required(True)
+def chat():
+
+    log.debug("start chat page..")
+    return render_template('chat_page.html')
+
+
+@chat_page.route('/data', methods=['POST'])
+@jwt_required(True)
+def get_data():
+
+    log.info("get chat message, request: %s, data: %s", request, request.data)
+
+    data = json.loads(request.data)
+
+
+    text = data.get('data')
+    user_input = text
+
+    log.debug("user_input: %s", user_input)
+
+    try:
+
+        # conversation = ConversationChain(llm=llm,memory=memory)
+        # output = conversation.predict(input=user_input)
+
+        # memory.save_context({"input": user_input}, {"output": output})
+
+        output = "response.."
+
+        return jsonify({"response":True, "message": output}), 200
+
+    except Exception as e:
+        log.error(e)
+        return jsonify({"message": f'Error: {str(e)}', "response":False}), 200
+
